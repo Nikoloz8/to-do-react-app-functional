3bcc767707da2e05d95b9ebd3e0e48d315efa538
Nikoloz Tevdoradze <nikoloztevdoradze08@gmail.com>
2025-05-11 16:27:33 +0400

    Commit

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,13 +1,18 @@
 import "./todo.css"
 
 export default function Todo({ todos, setDone, setTodos, done }) {
+
+    function handleDelete(ind) {
+        setTodos(todos.filter((_, i) => i !== ind))
+        setDone(done.filter(e => e !== ind))
+    }
+
     return (
         todos.map((e, index) => {
             return <div key={index} className='to-do-container'>
                 <p onClick={() => !done.includes(index) ? setDone([...done, index]) : setDone(done.filter(e => e !== index))} style={done.includes(index) ? { textDecorationLine: "line-through" } : { textDecorationLine: "none" }}>{e}</p>
                 <button onClick={() => {
-                    setTodos(todos.filter((_, i) => i !== index))
-                    setDone(done.filter(e => e !== index))
+                    handleDelete(index)
                 }}>Delete</button>
             </div>
         })
